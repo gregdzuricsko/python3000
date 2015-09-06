e97f797025b3c6bf4c9b15d11027bed30969e3cc
Greg Dzuricsko <gregdzuricsko@gmail.com>
2015-09-06 01:01:15 -0500

    basic styling

--- gayyyy/front/src/static/basic.js
+++ gayyyy/front/src/static/basic.js
@@ -3,6 +3,6 @@ import * as GayModule from './gay/gay_module';
 import angular from 'angular';
 console.log("help");
 
-angular.module('gay', [])
+var app = angular.module('gay', [])
     .factory('gaySvc', GayModule.svc)
     .controller('gayCtrl', GayModule.ctrl);
